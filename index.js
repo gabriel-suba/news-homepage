4b15e361ca7ca286bf23d6e7ba12abe24e89dc09
gab <wr3gab@gmail.com>
2022-10-25 10:16:29 +0800

    add overlay when menu is active

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const menu = document.querySelector('.menu-toggle')
 
 openBtn.addEventListener('click', () => {
 	menu.classList.toggle('menu-open')
+	menu.classList.toggle('overlay')
 })
 
 closeBtn.addEventListener('click', () => {
 	menu.classList.toggle('menu-open')
+	menu.classList.toggle('overlay')
 })
